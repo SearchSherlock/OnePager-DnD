4912bc9c4367aab47c33af1d43a44440de3ac3bc
SearchSherlock <harald@invadox.at>
2024-03-20 13:51:09 +0100

    formatierung der documente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var menuToggle = document.getElementById('menuToggle');
   var navMenu = document.getElementById('navMenu');
 

498c5b918bc18f000e056ba6cd773c84b9fdbf35
SearchSherlock <harald@invadox.at>
2024-03-27 13:10:19 +0100

    implement the right parallax 4 hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,28 +9,5 @@ function toggleNavMenu(navMenuElement) {
   navMenuElement.classList.toggle('nav-menu--open');
 }
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   var parallaxElements = document.querySelectorAll('.parallax');
-//   var speed = 0.5;
-
-//   window.addEventListener('scroll', function () {
-//     var offset = window.pageYOffset;
-
-//     parallaxElements.forEach(function (elem) {
-//       elem.style.backgroundPositionY = offset * speed + "px";
-//     });
-//   });
-// });
-
-document.addEventListener('DOMContentLoaded', function () {
-  var parallaxElements = document.querySelectorAll('.parllay');
-  var speed = 0.1;
-
-  window.addEventListener('scroll', function () {
-    var offset = window.pageYOffset;
-
-    parallaxElements.forEach(function (elem) {
-      elem.style.backgroundPositionY = offset * speed + "px";
-    });
-  });
-});
+var image = document.getElementById('hero-bg-image');
+new simpleParallax(image);

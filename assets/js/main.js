709e4995dda4c7d3b914d1039552e734686897eb
SearchSherlock <harald@invadox.at>
2024-03-27 11:47:37 +0100

    also changes from laptop

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,29 @@ document.addEventListener('DOMContentLoaded', function () {
 function toggleNavMenu(navMenuElement) {
   navMenuElement.classList.toggle('nav-menu--open');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  var parallaxElements = document.querySelectorAll('.parallax');
+  var speed = 0.5;
+
+  window.addEventListener('scroll', function () {
+    var offset = window.pageYOffset;
+
+    parallaxElements.forEach(function (elem) {
+      elem.style.backgroundPositionY = offset * speed + "px";
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var parallaxElements = document.querySelectorAll('.parllay');
+  var speed = 0.1;
+
+  window.addEventListener('scroll', function () {
+    var offset = window.pageYOffset;
+
+    parallaxElements.forEach(function (elem) {
+      elem.style.backgroundPositionY = offset * speed + "px";
+    });
+  });
+});
